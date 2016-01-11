9c13ff01b3794c6737c41bdf7f109a3ab77be86f
Wijtse Rekker <wijtse@rekkerit.nl>
2016-01-11 18:30:16 +0100

    board toString update

--- module2project/src/client/Board.java
+++ module2project/src/client/Board.java
@@ -5,22 +5,29 @@ import java.util.List;
 
 public class Board {
   
-  private List<List<Tile>> boardMatrix;
+  private ArrayList<ArrayList<Tile>> boardMatrix;
   
   /**
    * Board constructor. Constructs a empty board.
    */
   public Board() {
-    for (int row = 0; row < 182; row++) {
+    boardMatrix = new ArrayList<ArrayList<Tile>>();
+    for (int row = 0; row < 183; row++) {
       boardMatrix.add(new ArrayList<Tile>());
-      for (int collumn = 0; collumn < 182; collumn++) {
+      for (int column = 0; column < 183; column++) {
         boardMatrix.get(row).add(new Tile());
       }
     }
   }
   
+  public void put(Move move) {
+    int row = move.getRow();
+    int column = move.getColumn();
+    boardMatrix.get(row).set(column, move.getTile());
+  }
+  
   /**
-   * Calculates the part of the board where there are tiles
+   * Calculates the part of the board where there are tiles.
    * @return 
    */
   public ArrayList<ArrayList<Integer>> getMarges() {
@@ -34,7 +41,8 @@ public class Board {
     int columnMin = 0;
     int columnMax = 0;
     String empty = ". ";
-    while (boardMatrix.get(row).get(column).toString().equals(empty) && row != 182) { //Get the start row
+    //Get the start row
+    while (row < 182 && boardMatrix.get(row).get(column).toString().equals(empty)) { 
       if (column < 182) {
         column++;
       } else {
@@ -47,7 +55,8 @@ public class Board {
     
     row = 182;
     column = 0;
-    while (boardMatrix.get(row).get(column).toString().equals(empty) && row != 0) { //Get the end row
+    //Get the end row
+    while (row > 0 && boardMatrix.get(row).get(column).toString().equals(empty)) { 
       if (column > 0) {
         column--;
       } else {
@@ -60,7 +69,8 @@ public class Board {
     if (rowMin < rowMax) {
       row = 0;
       column = 0;
-      while (boardMatrix.get(row).get(column).toString().equals(empty) && column != 182) { //Get the start column
+      //Get the start column
+      while (column < 182 && boardMatrix.get(row).get(column).toString().equals(empty)) { 
         if (row < 182) {
           row++;
         } else {
@@ -72,7 +82,8 @@ public class Board {
       
       row = 0;
       column = 182;
-      while (boardMatrix.get(row).get(column).toString().equals(empty) && column != 0) { //Get the end row
+      //Get the end row
+      while (column > 0 && boardMatrix.get(row).get(column).toString().equals(empty)) { 
         if (row > 0) {
           row--;
         } else {
@@ -82,10 +93,11 @@ public class Board {
       }
       columnMax = column + 1;
     } else {
-      result.get(0).add(91);
-      result.get(0).add(93);
-      result.get(1).add(91);
-      result.get(1).add(93);
+      result.get(0).add(90);
+      result.get(0).add(92);
+      result.get(1).add(90);
+      result.get(1).add(92);
+      System.out.println("DINKIE");
     }
     result.get(0).add(rowMin);
     result.get(0).add(rowMax);
@@ -96,8 +108,32 @@ public class Board {
     return result;
   }
   
-  public Boolean checkMove(Tile tile, int row, int collumn) {
+  public String toString() {
+    String result = "";
+    ArrayList<ArrayList<Integer>> marges = getMarges();
+    int rowMin = marges.get(0).get(0);
+    int rowMax = marges.get(0).get(1);
+    int columnMin = marges.get(1).get(0);
+    int columnMax = marges.get(1).get(1);
+    for (int row = rowMin; row <= rowMax; row++) {
+      for (int column = columnMin; column <= columnMax; column++) {
+        result = result + boardMatrix.get(row).get(column).toString();
+      }
+      result = result + "\n";
+    }
+    return result;
+  }
+  
+  public Boolean checkMove(Move move) {
     return true;
+  }
+  
+  public static void main(String[] args) {
+    Board board = new Board();
+    Tile tile = new Tile("A", "B");
+    Move move = new Move(tile, 91, 91);
+    board.put(move);
+    System.out.print(board.toString());
   }
   
 }
