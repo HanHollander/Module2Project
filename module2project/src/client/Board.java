abbce80705232c0ffc99e13c2a9d32edada9d594
Wijtse Rekker <wijtse@rekkerit.nl>
2016-01-11 15:42:27 +0100

    board tile update van da wijtsuh

--- module2project/src/client/Board.java
+++ module2project/src/client/Board.java
@@ -1,12 +1,31 @@
 package client;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Board {
   
-  private ArrayList<List<Tile>> boardMatrix;
+  private List<List<Tile>> boardMatrix;
   
-  public void Board() {
-    
+  /**
+   * Board constructor. Constructs a empty board.
+   */
+  public Board() {
+    for (int row = 0; row < 185; row++) {
+      boardMatrix.add(new ArrayList<Tile>());
+      for (int collumn = 0; collumn < 185; collumn++) {
+        boardMatrix.get(row).add(new Tile());
+      }
+    }
   }
+  
+  public List<List<Integer>> getMarges() {
+    
+    return null;
+  }
+  
+  public Boolean checkMove(Tile tile, int row, int collumn) {
+    return true;
+  }
+  
 }
